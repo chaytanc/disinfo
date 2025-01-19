df3171f746207abf7a19492fa2d60170e173068d
chaytanc <35582442+chaytanc@users.noreply.github.com>
2025-01-18 16:37:52 -0800

    Now we're cooking

--- trumpalyze.py
+++ trumpalyze.py
@@ -9,7 +9,8 @@ import numpy as np
 
 # TODO params file yaml
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-file = "trumptweets1205-127.csv"
+#file = "trumptweets1205-127.csv"
+file = "syria_articles/wsj_article.txt"
 n_tweets = 1000
 # Do not change the order of this array
 narratives = ["America is too tolerant of foreigners and nontraditional behaviors", "America needs strong men to lead it", "Russia is an ally"]
@@ -17,37 +18,28 @@ narratives = ["America is too tolerant of foreigners and nontraditional behavior
 pd.options.display.float_format = '{:.2f}'.format
 np.set_printoptions(precision=2, suppress=True)
 
-# class Result():
-#     def __init__(self, tweet, similarities, sim_ind):
-#         self.tweet = tweet
-#         self.similarities = similarities # np array
-#         self.max_sim = np.max(similarities)
-#         self.sim_ind = sim_ind
-#         #self.min_sim = min(similarities, key=lambda similarity: similarity[1])
-
-#     def __repr__(self):
-#         return f"Result(tweet='{self.tweet}', sims={self.similarities})\n"
-    
-#     def __eq__(self, other):
-#         return self.tweet == other.tweet
-    
-#     # This sorting style implies we primarily care if *any* score is high, not just a particular one or all of them
-#     def __lt__(self, other):
-#         return self.max_sim < other.max_sim
-
-# If you want to save some semblance of OOP but still have numpy, you can have results class instead of indiv results obj
-# might still keep results obj and just have ind reference to where it's located in similarities...? that mihgt be useful later
 class Results():
     def __init__(self, tweet_file, n_tweets, narratives):
-        self.df = pd.read_csv(tweet_file, encoding='utf-8', encoding_errors='ignore')
-        # self.similarities = np.empty((n_tweets, len(narratives)))
-        # self.results = []
-        self.results = pd.DataFrame(columns=["Tweet", "Similarities", "Index"])
+        self.read_media(tweet_file)
+        if n_tweets > len(self.df):
+            n_tweets = len(self.df)
+            print("Number of tweets selected is greater than total.\
+                  \nContinuing with {num} tweets total.".format(num=n_tweets))
         self.n_tweets = n_tweets
+        self.similarities = np.empty((self.n_tweets, len(narratives)))
+        self.tweets = pd.DataFrame(columns=["Tweet", "Sim_Index"])
         self.narratives = narratives
         # This should really be called by the user but what good is the results class if it has none ?
         self.get_results()
 
+    def read_media(self, file):
+        if file.endswith(".csv"):
+            self.df = pd.read_csv(file, encoding='utf-8', encoding_errors='ignore')
+        elif file.endswith(".txt"):
+            with open(file, "r") as f:
+                content = f.read()
+                self.df = pd.DataFrame({"Tweet": [content]})
+            
     # TODO -- leave model a global?
     def embed_narratives(self, narratives):
         nar_embeds = []
@@ -57,34 +49,51 @@ class Results():
 
     def get_results(self):
         nar_embeds = self.embed_narratives(self.narratives)
-        # TODO could make df indexing parametrized or could just make unique analysis funcs (probably cleaner, though less modular)
         for i, tweet in enumerate(self.df["Tweet"][:self.n_tweets]):
-            embedding= model.encode(tweet, convert_to_tensor=True)
+            embedding = model.encode(tweet, convert_to_tensor=True)
             tweet_sims = np.empty(len(nar_embeds))
             for j, nar_embed in enumerate(nar_embeds):
                 sim = util.pytorch_cos_sim(embedding, nar_embed)
                 tweet_sims[j] = sim
-            # self.similarities[i] = tweet_sims
-            # self.results.append(Result(tweet, tweet_sims, i))
-            self.results.loc[i] = {"Tweet" : tweet, "Similarities": tweet_sims, "Index" : i} # TODO Store index for later?
+            self.similarities[i] = tweet_sims
+            self.tweets.loc[i] = {"Tweet" : tweet, "Sim_Index" : i}
+
+    def sort_by_narrative(self, narrative_ind):
+        if narrative_ind > len(self.narratives) - 1:
+            print("Invalid narrative index. Continuing with narrative_ind=0...")
+            narrative_ind = 0
+        # Grab the column of the narrative_ind
+        narr_sims = self.similarities.T[narrative_ind] # after we transpose, we have 3 rows and n_tweets cols
+        sorted_args = np.argsort(narr_sims)
+        # get the sorted sims and the sorted tweets
+        sorted_sims = narr_sims[sorted_args]
+        sorted_tweets = self.tweets.iloc[sorted_args]
+        return sorted_tweets, sorted_sims
+
+    def print_top_k(self, k, narrative_ind):
+        if k > self.n_tweets:
+            k = self.n_tweets
+        if narrative_ind > len(self.narratives) - 1:
+            print("Invalid narrative index. Continuing with narrative_ind=0...")
+            narrative_ind = 0
+        sorted_tweets, sorted_sims = self.sort_by_narrative(narrative_ind)
+        sorted_tweets["Sims"] = sorted_sims
+        pd.set_option('display.max_colwidth', None)
+        print("{k} Most similar tweets to narrative \n\"{narrative}\": \n".format(k=k, narrative=self.narratives[narrative_ind]), 
+              sorted_tweets[-k:])
+
+    #def print_results(self, inds):
+        #print("Tweets: ", self.tweets[inds])
+        #print("Similarities Matrix (n_tweets x # narratives): ", self.similarities[inds])
 
     def __repr__(self):
-        return f"First 10 Results: \n {self.results[:10]}"
+        return f"First 10 Results: \n {self.tweets[:10]}"
 
 # Show results with highest similarity ratings in any narrative dimension
 results = Results(file, n_tweets, narratives)
 print(results)
-print(results.results["Similarities"])
-sorted_results = results.results.sort_values(by="Similarities", key=lambda col: col.apply(np.max))
-print("Lowest sims: ", sorted_results[:10])
-print("Highest sims: ", sorted_results[-10:])
-
-# What if I want a function that takes in a particular narrative and sorts based on the highest in that category
-# TODO use similarities matrix
-def sort_by_narrative(narrative_ind, results):
-    narrative_sims = results.results["Similarities"].apply(lambda sim_array: sim_array[narrative_ind])
-    print(narrative_sims.sort_values())
-    return sorted(narrative_sims)
+#sort_by_narrative(0, results)
+results.print_top_k(k=10, narrative_ind=3)
 
 # Use an LLM summary to generate possible narratives
 # https://huggingface.co/Ayush-1722/Meta-Llama-3-8B-Instruct-Summarize-v0.2-16K-LoRANET-Merged
@@ -97,3 +106,5 @@ def sort_by_narrative(narrative_ind, results):
 # Go paragraph by paragraph in article and add up similarity score?
 
 # TODO is there a way to inject geopolitical context into comparison? dates & headlines?
+
+# TODO more data
